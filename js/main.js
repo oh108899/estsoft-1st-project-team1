19ebe0a62219c74727857946b499ab3bf0a433db
oh108899 <oh108899@gmail.com>
2025-11-28 15:53:14 +0900

    feat:팝업창 js 구현

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const popup = document.querySelector('.popup');
+const popupBtn = popup.querySelector('.close');
+const check = popup.querySelector('#agree')
+const ok = document.cookie.includes('popup=ok')
+
+if(ok){
+  popup.style.display = 'none';
+}else{
+  popup.style.display = 'block';
+}
+popupBtn.addEventListener('click',()=>{
+  popup.style.display = 'none';
+  if(check.checked){
+    createCookie('popup','ok',1);
+  }else{
+    createCookie('popup','ok',-1);
+  }
+})
+
+function createCookie(name,value,day){
+  let today = new Date();
+  today.setDate(today.getDate()+day);
+  document.cookie = `${name}=${value}; Expires=${today.toUTCString()}`
+}
